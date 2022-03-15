0ef4d1765da34f183fb8fa067e5b7b2006536c39
Yann MARCOU <creativeyann17@gmail.com>
2022-03-15 12:09:17 -0400

    feat: CLIN-842 - add truststore + robustness

--- src/main/scala/ca/ferlab/ferload/client/Main.scala
+++ src/main/scala/ca/ferlab/ferload/client/Main.scala
@@ -4,11 +4,14 @@ import ca.ferlab.ferload.client.clients.{ConsoleCommandLine, FerloadClient, Keyc
 import ca.ferlab.ferload.client.commands.factory.CommandFactory
 import ca.ferlab.ferload.client.commands.{Configure, Download}
 import ca.ferlab.ferload.client.configurations.UserConfig
+import com.amazonaws.SDKGlobalConfiguration
 import com.typesafe.config.{Config, ConfigFactory}
+import org.apache.commons.io.FileUtils
 import picocli.CommandLine
 import picocli.CommandLine.Command
 
 import java.io.File
+import scala.util.{Failure, Success, Using}
 
 @Command(name = "ferload-client", mixinStandardHelpOptions = true,
   version = Array("0.1"),
@@ -21,6 +24,20 @@ class Main extends Runnable {
 }
 
 object Main {
+  
+  Using(this.getClass.getClassLoader.getResourceAsStream("cacerts.jks")) { is =>
+    val tempFile = File.createTempFile("cacerts", ".jks")
+    FileUtils.copyInputStreamToFile(is, tempFile)
+    System.setProperty(SDKGlobalConfiguration.DISABLE_CERT_CHECKING_SYSTEM_PROPERTY, "true")  // aws client
+    System.setProperty("javax.net.ssl.trustStore", tempFile.getPath)  // keycloak / ferload
+    System.setProperty("javax.net.ssl.trustStorePassword", "changeit")
+    System.setProperty("javax.net.ssl.trustStoreType", "jks")
+    tempFile.getPath
+  } match {
+    case Success(_) =>
+    case Failure(e) => throw new IllegalStateException(s"Failed to load certificates: " + e.getMessage, e)
+  }
+  
   def main(args: Array[String]): Unit = {
     val userHome: String = System.getProperty("user.home")
     val baseConfig = ConfigFactory.load
